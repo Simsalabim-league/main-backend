021edcf7626feb1a905d4a0998b972aaa1777913
Maxim Yakovlev <max.yakovlev555@gmail.com>
2021-02-16 10:42:51 +0300

    Remove unused interface

--- src/utils/createHandler.ts
+++ src/utils/createHandler.ts
@@ -1,12 +1,9 @@
 import { tryCatch } from 'fp-ts/TaskEither'
 
-import { Option, Some } from 'fp-ts/lib/Option'
-import { IVerifyOptions } from 'passport-local'
+import { Some } from 'fp-ts/lib/Option'
 import { NextFunction, Request, Response } from 'express'
 import { ApiError, IApiError } from './throwApiError'
 import { User } from '@src/types/user'
-
-export type DoneeFn = (error: unknown, user?: Option<User>, options?: IVerifyOptions & { status: 'failed'|'done'; }) => void
 
 export type ApiHandler<P = unknown, RSB = unknown, RQB = unknown, RQ = unknown> = (
     req: Request<P, RSB, RQB, RQ> & { user: Some<User>; },
